62fe875535a55661e69274ab5599f59c4421765b
fuyunfan <yunfan.fu@biomind.ai>
2022-06-21 17:22:23 +0800

    init

--- protos/logger/metadata.go
+++ protos/logger/metadata.go
@@ -7,7 +7,7 @@ import (
 func Metadata() *registry.Metadata {
 	return &registry.Metadata{
 		ServiceDesc: LoggerService_ServiceDesc,
-		Namespace:   "biomind",
+		Namespace:   "neptune",
 		Version:     "v1",
 	}
 }
